1c827bcad8e778a6329d3aa816357ad652f66c11
Benjamin Brücker <bb@benjaminbruecker.de>
2025-08-21 21:38:51 +0200

    Lauffähige Version

--- clone-containers.sh
+++ clone-containers.sh
@@ -14,10 +14,41 @@ clone_wrapper() {
 
     # Prüfe ob eine Instanz übersprungen werden soll
     if [ "${SKIP:-}" == "true" ]; then
-        echo "[INFO] Überspringe: '$NAME'"
+        echo "[INFO] Überspringe: '$STACK'"
         return 0
     fi
-    
+
+    # Instanz stoppen
+    docker compose -f "$RC_SOURCE_FOLDER/docker-compose.yml" down
+    if [ $? -ne 0 ]; then
+        echo "[ERROR] Konnte Instanz nicht stoppen wechseln: $STACK"
+        return 1
+    else
+        echo "[INFO] Instanz gestoppt: $STACK"
+    fi
+
+    # Perform synchronization
+    echo "[INFO] Sync $STACK"
+    echo "--------------------------------------------------------"
+    rclone sync "${RC_EXCEPTIONS[@]}" $RC_PARAMS "$RC_SOURCE_FOLDER" "$RC_REMOTE_NAME":"$RC_REMOTE_FOLDER" || error_continue
+    echo "--------------------------------------------------------"
+
+    # Perform check.
+    echo "[INFO] Check $STACK"
+    echo "--------------------------------------------------------"
+    rclone check "${RC_EXCEPTIONS[@]}" $RC_PARAMS "$RC_SOURCE_FOLDER" "$RC_REMOTE_NAME":"$RC_REMOTE_FOLDER" || error_continue
+    echo "--------------------------------------------------------"
+
+    # Instanz starten
+    echo "[INFO] Starte Instanz: $STACK"
+    docker compose -f "$RC_SOURCE_FOLDER/docker-compose.yml" up -d
+    if [ $? -ne 0 ]; then
+        echo "[ERROR] Konnte Instanz nicht starten: $STACK"
+        return 1
+    else
+        echo "[INFO] Instanz gestartet: $STACK"
+    fi
+
 }
 
 # Prüfe, ob Konfigurationsdateien vorhanden sind
